2cd0eff4eb3c1d35946a03dfa0d2b55aec056e40
runi95 <runar.serigstad@gmail.com>
2022-04-03 21:26:44 +0200

    Added missing semicolon

--- src/Creeps/Normal/PinkVillager.ts
+++ src/Creeps/Normal/PinkVillager.ts
@@ -4,6 +4,6 @@ import { YellowVillager } from "./YellowVillager";
 
 export class PinkVillager extends Creep {
     public speed = 3.5;
-    protected readonly children: Creep[] = [new YellowVillager()]
+    protected readonly children: Creep[] = [new YellowVillager()];
     protected readonly color: Color = {r: 255, g: 125, b: 255, a: 255};
 }
