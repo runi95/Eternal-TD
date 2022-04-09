0311f53e8a0d3ebe2d001d1d7d81e2dbb6ed4e52
runi95 <runar.serigstad@gmail.com>
2022-04-09 12:31:00 +0200

    Added missing semicolon

--- src/Towers/TowerUpgrade.ts
+++ src/Towers/TowerUpgrade.ts
@@ -3,7 +3,7 @@ export abstract class TowerUpgrade {
     public abstract icon: string;
     public abstract cost: number;
     public abstract description: string;
-    public abstract applyUpgrade(unit: unit): void
+    public abstract applyUpgrade(unit: unit): void;
     
     public newUnitTypeId?: number = undefined;
 }
