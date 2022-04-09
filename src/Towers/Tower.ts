bebda1c192ea624b61f48b0dd95e364ddc7c30e3
runi95 <runar.serigstad@gmail.com>
2022-04-09 12:32:53 +0200

    Removed unnecessary comma

--- src/Towers/Tower.ts
+++ src/Towers/Tower.ts
@@ -5,7 +5,7 @@ export class Tower {
     public readonly unit: unit;
     public readonly towerType: TowerType;
 
-    constructor(unit: unit, towerType: TowerType, pathUpgrades: number[] = [0, 0, 0],) {
+    constructor(unit: unit, towerType: TowerType, pathUpgrades: number[] = [0, 0, 0]) {
         this.unit = unit;
         this.towerType = towerType;
         this.pathUpgrades = pathUpgrades;
