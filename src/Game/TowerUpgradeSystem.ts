4f4d2d85b510f0dc494e6906f460c73e00e0e409
runi95 <runar.serigstad@gmail.com>
2022-04-24 10:58:30 +0200

    Removed commented out code

--- src/Game/TowerUpgradeSystem.ts
+++ src/Game/TowerUpgradeSystem.ts
@@ -16,9 +16,6 @@ export class TowerUpgradeSystem {
     private readonly upgradePathTextFrames: Frame[][];
     private readonly upgradePathEnabled: boolean[][];
 
-    // private readonly towerIconFrame: framehandle;
-    // private readonly sellText: framehandle;
-
     constructor(towerController: TowerController, towers: Map<number, Tower>) {
         this.towerController = towerController;
         this.towers = towers;
@@ -54,85 +51,10 @@ export class TowerUpgradeSystem {
             }
         }
 
-        // BlzFrameSetSize(path1Tier1ButtonFrame, 0.02625, 0.02625);
-
-        // BlzFrameSetTexture(this.path2Tier1IconFrame, "war3mapImported/TowerUpgradePanel.dds", 0, true);
-
-        // this.menu = BlzCreateSimpleFrame('SimpleObserverPanel', this.originFrameGameUi, 0);
-        // BlzFrameSetSize(this.menu, 0.22, 0.48);
-        // BlzFrameSetPoint(this.menu, FRAMEPOINT_RIGHT, this.originFrameGameUi, FRAMEPOINT_RIGHT, 0, 0);
-
-        // Upgrade menu
-        // this.menu = BlzCreateSimpleFrame('MinimapButtonBar', this.originFrameGameUi, 0);
-
-        // BlzFrameSetSize(this.menu, 0.22, 0.48);
-        // BlzFrameSetPoint(this.menu, FRAMEPOINT_RIGHT, this.originFrameGameUi, FRAMEPOINT_RIGHT, 0, 0);
-        // BlzFrameSetPoint(this.menu, FRAMEPOINT_CENTER, this.originFrameGameUi, FRAMEPOINT_CENTER, 0.4, 0.5);
-
-        /*
-        // Menu backdrop
-        const menuBackdrop: framehandle = BlzCreateFrame('EscMenuBackdrop', this.menu, 0, 0);
-        BlzFrameSetAllPoints(menuBackdrop, this.menu);
-
-        this.path2Tier1IconFrame = BlzCreateFrameByType('BACKDROP', 'path2Tier1IconFrame', this.menu, '', 0);
-        BlzFrameSetSize(this.path2Tier1IconFrame, 0.04, 0.04);
-        BlzFrameSetPoint(this.path2Tier1IconFrame, FRAMEPOINT_BOTTOMLEFT, this.menu, FRAMEPOINT_BOTTOMLEFT, 0.035, 0.06);
-        this.path2Tier2IconFrame = BlzCreateFrameByType('BACKDROP', 'path2Tier2IconFrame', this.menu, '', 0);
-        BlzFrameSetSize(this.path2Tier2IconFrame, 0.04, 0.04);
-        BlzFrameSetPoint(this.path2Tier2IconFrame, FRAMEPOINT_BOTTOM, this.menu, FRAMEPOINT_BOTTOM, -0.0051, 0.06);
-        this.path2Tier3IconFrame = BlzCreateFrameByType('BACKDROP', 'path2Tier3IconFrame', this.menu, '', 0);
-        BlzFrameSetSize(this.path2Tier3IconFrame, 0.04, 0.04);
-        BlzFrameSetPoint(this.path2Tier3IconFrame, FRAMEPOINT_BOTTOMRIGHT, this.menu, FRAMEPOINT_BOTTOMRIGHT, -0.047, 0.06);
-        */
-
-
-
-
-        
-        // Menu title
-        // const menuTitle: framehandle = BlzCreateFrame('StandardTitleTextTemplate', this.menu, 0, 0);
-        // BlzFrameSetPoint(menuTitle, FRAMEPOINT_TOP, this.menu, FRAMEPOINT_TOP, 0.0, -0.04);
-
-        // Menu escape button
-        // const closeButton: framehandle = BlzCreateFrameByType('GLUETEXTBUTTON', 'closeButton', this.menu, 'ScriptDialogButton', 0);
-        // BlzFrameSetPoint(closeButton, FRAMEPOINT_BOTTOM, this.menu, FRAMEPOINT_BOTTOM, 0.0, 0.025);
-        // BlzFrameSetText(closeButton, 'Close');
-
-        // Tower icon frame
-        // this.towerIconFrame = BlzCreateFrameByType('BACKDROP', 'towerIconFrame', this.menu, '', 0);
-        // BlzFrameSetSize(this.towerIconFrame, 0.04, 0.04);
-        // BlzFrameSetPoint(this.towerIconFrame, FRAMEPOINT_LEFT, this.menu, FRAMEPOINT_LEFT, 0.04, 0.06);
-        // BlzFrameSetAllPoints(this.towerIconFrame, this.towerIconFrame);
-
-        // const towerIconHighlightFrame = BlzCreateFrame('ScoreScreenTabButtonBackdrop', this.towerIconFrame, 0, 0);
-        // const towerIconHighlightFrame = BlzCreateFrame('BattleNetButtonMouseOverHighlightTemplate', this.towerIconFrame, 0, 0);
-        // const towerIconHighlightFrame = BlzCreateFrame('BattleNetButtonFocusHighlightTemplate', this.towerIconFrame, 0, 0);
-        // BlzFrameSetAllPoints(towerIconHighlightFrame, this.towerIconFrame);
-
-        // const sellButton: framehandle = BlzCreateFrameByType('GLUETEXTBUTTON', 'sellButton', this.towerIconFrame, 'ScriptDialogButton', 0);
-        // BlzFrameSetPoint(sellButton, FRAMEPOINT_BOTTOM, this.towerIconFrame, FRAMEPOINT_BOTTOM, 0.0, -0.035);
-        // BlzFrameSetSize(sellButton, 0.05, 0.034);
-        // BlzFrameSetText(sellButton, 'Sell');
-
-        // this.sellText = BlzCreateFrameByType('TEXT', 'sellText', sellButton, '', 0);
-        // BlzFrameSetPoint(this.sellText, FRAMEPOINT_BOTTOM, sellButton, FRAMEPOINT_BOTTOM, 0, -0.01);
-        
-        // const closeButtonTrigger: Trigger = new Trigger();
-        // closeButtonTrigger.registerFrameEvent(closeButton, FRAMEEVENT_CONTROL_CLICK);
-        // closeButtonTrigger.registerPlayerKeyEvent(Player(0), OSKEY_ESCAPE, 0, true);
-        // closeButtonTrigger.addAction(() => {
-        //     BlzFrameSetVisible(this.menu, false);
-        // });
-
-        // BlzFrameSetVisible(this.menu, false);
-
         const selectUnitTrig: Trigger = new Trigger();
         selectUnitTrig.addAction(() => {
             const trig: unit = GetTriggerUnit();
             const unitHandleId: number = GetHandleId(trig);
-            // const trigUnitTypeId: number = GetUnitTypeId(trig);
-            // const goldCost: number = GetUnitGoldCost(trigUnitTypeId);
-            // BlzFrameSetText(this.sellText, `(|cFFFFCC00${goldCost}|r)`);
 
             const tower: Tower | undefined = this.towers.get(unitHandleId);
             if (tower === undefined) {
@@ -185,7 +107,6 @@ export class TowerUpgradeSystem {
     private createUpgradePathButtonFrame(parent: Frame): [Frame, Frame] {
         const buttonFrame = new Frame("buttonFrame", parent, -1, 0, 'BUTTON', '');
         buttonFrame.setAllPoints(parent);
-        // const tooltipFrame = BlzCreateFrame("StandardDecoratedEditBoxBackdropTemplate", buttonFrame, 0, 0);
         const tooltipFrame = new Frame("BoxedText", buttonFrame, 0, 0);
         const textFrame = new Frame("textFrame", tooltipFrame, -1, 0, "TEXT", '');
         textFrame.setSize(0.25, 0)
