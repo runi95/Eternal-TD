d2ada5b884a05ecf7e4a44de85e413cd29de4211
runi95 <runar.serigstad@gmail.com>
2022-06-26 12:12:25 +0200

    Removed the custom unit for `LargeVillager` and replaced it with a `Scale modifier` instead

--- src/Creeps/Normal/LargeVillager.ts
+++ src/Creeps/Normal/LargeVillager.ts
@@ -1,13 +1,13 @@
 import { CreepBaseUnit } from "Creeps/CreepBaseUnit";
+import { Scale } from "Utility/Scale";
 import { Color } from "../../Utility/Color";
 import { RainbowVillager } from "./RainbowVillager";
 
-const largeVillagerTypeId: number = FourCC('u005');
 export class LargeVillager extends CreepBaseUnit {
     public static readonly LARGE_VILLAGER = new LargeVillager();
 
     public readonly health: number = 20;
-    public readonly unitTypeId: number = largeVillagerTypeId;
     public readonly color: Color = {r: 255, g: 255, b: 255, a: 255};
+    public readonly scale: Scale = {x: 1.5, y: 1.5, z: 2};
     public readonly children: CreepBaseUnit[] = [RainbowVillager.RAINBOW_VILLAGER, RainbowVillager.RAINBOW_VILLAGER];
 }
