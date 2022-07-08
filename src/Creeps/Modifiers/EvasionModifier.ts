653ac074a49ae4cc78f34238b28fea020783ba44
runi95 <runar.serigstad@gmail.com>
2022-07-08 20:33:55 +0200

    Created a new modifier `EvasionModifier`

---
+++ src/Creeps/Modifiers/EvasionModifier.ts
@@ -0,0 +1,13 @@
+import { CreepModifier } from "Creeps/CreepModifier";
+import { Unit } from "w3ts";
+
+const evasionAbilityId: number = FourCC('A00F');
+export class EvasionModifier extends CreepModifier {
+    public static readonly EVASION_MODIFIER = new EvasionModifier();
+
+    public readonly icon: string = ""; // TODO: Find icon
+    public readonly description: string = ""; // TODO: Write description
+    public readonly applyEffect = (unit: Unit) => {
+        unit.addAbility(evasionAbilityId);
+    };
+};
