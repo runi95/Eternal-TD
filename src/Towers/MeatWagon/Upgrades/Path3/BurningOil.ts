244a3e4a1578ee94a24eddf272068003646a6a83
runi95 <runar.serigstad@gmail.com>
2022-08-13 01:34:08 +0200

    Updated icon for `BurningOil`

--- src/Towers/MeatWagon/Upgrades/Path3/BurningOil.ts
+++ src/Towers/MeatWagon/Upgrades/Path3/BurningOil.ts
@@ -4,7 +4,7 @@ import type { MeatWagoCustomData } from "../../MeatWagon";
 
 export class BurningOil extends TowerUpgrade {
     public name = "Burning Oil";
-    public icon = "ReplaceableTextures/CommandButtons/BTNBurningOil.blp";
+    public icon = "ReplaceableTextures/CommandButtons/BTNIncinerate.blp";
     public cost = 43200;
     public description = "TODO: Write description";
 
