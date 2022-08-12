97cf574b43a46ffacc5bf741e04cb95c541e4c4d
runi95 <runar.serigstad@gmail.com>
2022-08-12 20:07:36 +0200

    Updated AOE for `MeatWagon` is now properly displayed by the `AttackGround` ability

--- src/Towers/MeatWagon/Upgrades/Path1/BiggerBlast.ts
+++ src/Towers/MeatWagon/Upgrades/Path1/BiggerBlast.ts
@@ -1,6 +1,7 @@
 import { Tower } from "../../../Tower";
 import { TowerUpgrade } from "../../../TowerUpgrade";
 
+const attackGroundAbilityId: number = FourCC('A00B');
 export class BiggerBlast extends TowerUpgrade {
     public name = "Bigger Blast";
     public icon = "ReplaceableTextures/CommandButtons/BTNSmash.blp";
@@ -8,9 +9,13 @@ export class BiggerBlast extends TowerUpgrade {
     public description = "TODO: Write description";
 
     public applyUpgrade(tower: Tower): void {
+        const newAreaOfEffect = 300;
+
         // NOTE: For some reason setting UNIT_WEAPON_RF_ATTACK_AREA_OF_EFFECT_FULL_DAMAGE also updates the attack cooldown so we have to remember it and re-set the value afterwards. 
         const currentCooldown: number = tower.unit.getAttackCooldown(0);
-        BlzSetUnitWeaponRealField(tower.unit.handle, UNIT_WEAPON_RF_ATTACK_AREA_OF_EFFECT_FULL_DAMAGE, 0, 300);
+        BlzSetUnitWeaponRealField(tower.unit.handle, UNIT_WEAPON_RF_ATTACK_AREA_OF_EFFECT_FULL_DAMAGE, 0, newAreaOfEffect);
         tower.unit.setAttackCooldown(currentCooldown, 0);
+
+        BlzSetAbilityRealLevelField(BlzGetUnitAbility(tower.unit.handle, attackGroundAbilityId), ABILITY_RLF_AREA_OF_EFFECT, 0, newAreaOfEffect);
     }
 }
--- src/Towers/MeatWagon/Upgrades/Path1/Shockwave.ts
+++ src/Towers/MeatWagon/Upgrades/Path1/Shockwave.ts
@@ -2,6 +2,7 @@ import { Tower } from "../../../Tower";
 import { TowerUpgrade } from "../../../TowerUpgrade";
 import { MeatWagoCustomData } from "../../MeatWagon";
 
+const attackGroundAbilityId: number = FourCC('A00B');
 export class Shockwave extends TowerUpgrade {
     public name = "Shockwave";
     public icon = "ReplaceableTextures/CommandButtons/BTNShockWave.blp";
@@ -10,10 +11,13 @@ export class Shockwave extends TowerUpgrade {
 
     public applyUpgrade(tower: Tower): void {
         (tower.customData as MeatWagoCustomData).hasShockwave = true;
+        const newAreaOfEffect = 450;
 
         // NOTE: For some reason setting UNIT_WEAPON_RF_ATTACK_AREA_OF_EFFECT_FULL_DAMAGE also updates the attack cooldown so we have to remember it and re-set the value afterwards. 
         const currentCooldown: number = tower.unit.getAttackCooldown(0);
-        BlzSetUnitWeaponRealField(tower.unit.handle, UNIT_WEAPON_RF_ATTACK_AREA_OF_EFFECT_FULL_DAMAGE, 0, 450);
+        BlzSetUnitWeaponRealField(tower.unit.handle, UNIT_WEAPON_RF_ATTACK_AREA_OF_EFFECT_FULL_DAMAGE, 0, newAreaOfEffect);
         tower.unit.setAttackCooldown(currentCooldown, 0);
+
+        BlzSetAbilityRealLevelField(BlzGetUnitAbility(tower.unit.handle, attackGroundAbilityId), ABILITY_RLF_AREA_OF_EFFECT, 0, newAreaOfEffect);
     }
 }
