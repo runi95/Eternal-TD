3b7b146fd61c46c06adb2051667f5376a6ba4da6
runi95 <runar.serigstad@gmail.com>
2022-08-17 23:45:33 +0200

    Added print to the global lua test types

--- test/overrides/lua-types.ts
+++ test/overrides/lua-types.ts
@@ -7,3 +7,9 @@ declare global {
 global.MathRound = function MathRound(v: number) {
     return Math.round(v);
 };
+
+global.print = (...args: any[]) => {
+    if (process.env.SILENT_MODE !== "true") {
+        console.log(...args);
+    }
+};
